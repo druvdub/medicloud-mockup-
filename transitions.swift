96fb3c2b39eaf8319cbad6d9f51a614e545a28d6
Dhruv <59387969+druvdub@users.noreply.github.com>
2020-12-14 19:28:16 +0530

    Create transitions.swift

---
+++ transitions.swift
@@ -0,0 +1,125 @@
+// On Mouse Enter - Navigation
+Animation.timingCurve(
+  0.12, 0.23, 0.5, 1,
+  duration: 0.3
+)
+  .delay(0.1)
+// Ontap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.4
+)
+  .delay(0.1)
+// scaleTransition
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 600,
+  damping: 30
+)
+// colorTransition
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Click - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// scaleTransition
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 600,
+  damping: 30
+)
+// colorTransition
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Click - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// scaleTransition
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 600,
+  damping: 30
+)
+// colorTransition
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Tap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Tap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Tap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Tap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Tap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Tap - Navigation
+Animation.timingCurve(
+  0.44, 0, 0.56, 1,
+  duration: 0.3
+)
+// On Tap - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Tap - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Tap - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Tap - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Tap - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
+// On Tap - Navigation
+Animation.interpolatingSpring(
+  mass: 1,
+  stiffness: 500,
+  damping: 60
+)
