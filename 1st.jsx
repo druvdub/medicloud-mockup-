c3760cf5b2d7cd020c2f91f6d3c3444d97303588
Dhruv <59387969+druvdub@users.noreply.github.com>
2020-12-14 19:31:46 +0530

    Create 1st.jsx

---
+++ 1st.jsx
@@ -0,0 +1,30 @@
+const $1stScreen = {
+  width: 414,
+  height: 896,
+  overflow: "hidden",
+  background: "linear-gradient(180deg, #002136 0%, #002b38 100%)",
+}
+
+const mEDICLOUD = {
+  width: 325,
+  height: 76,
+  overflow: "hidden",
+  fontFamily: `"Poppins", serif`,
+  color: "var(--Red_10_V02, #fff1f1)",
+  fontSize: 42,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+  fontWeight: 600,
+  fontStyle: "normal",
+}
+
+const asset28 = {
+  width: 305,
+  height: 305,
+  overflow: "visible",
+  backgroundImage: "url(/* Asset 2-8.png */)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
